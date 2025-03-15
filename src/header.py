50c746bfc590fdb64390aa180c95ff3ed5af85e9
Egor Sazonov <147899660+EgorSazonov0405@users.noreply.github.com>
2025-03-15 15:21:13 +0300

    upd: header.py

--- src/header.py
+++ src/header.py
@@ -7,20 +7,9 @@ router = APIRouter(
 
 
 @router.get("",
-            summary="Get User",
-            description="Get User")
-def get_user():
-    return {
-        "id": "<USER_ID>",
-        "name": "<USER_NAME>",
-        "avatar": "<USER_AVATAR>"
-    }
-
-
-@router.get("/{user_id}",
             summary="Get Unique User",
             description="Just a basic information fot the header user button")
-def get_unigue_user(user_id: int):
+def get_unigue_user(user_id: str = '<YOUR_ID>'):
     return {
         "id": user_id,
         "name": "<NAME>",
